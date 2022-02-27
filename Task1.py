676808730d77b5df17b09ac423177558ab450442
sherelene <71803296+sherelene@users.noreply.github.com>
2022-02-27 14:53:40 -0800

    filled in some of the functions

--- Task1.py
+++ Task1.py
@@ -6,17 +6,30 @@ def get_num_of_characters(characters):
     return amount
 
 
-def output_without_whitespace(alphanum):
+def output_without_whitespace(characters):
     i = 0
-    while i < len(alphanum):
-        if alphanum[i] == " " or alphanum[i] == "\t":
-            alphanum = alphanum[:i] + alphanum[i + 1:]
+    while i < len(characters):
+        if characters[i] == " " or characters[i] == "\t":
+            characters = characters[:i] + characters[i + 1:]
         else:
             i += 1
-    return alphanum
+    return characters
 
 
-#def get_acronym(acronym):
+def get_acronym(characters):
+    #-----------------original code but did not work if there were multiple " " or \t-----#
+    # add first letter
+    #acronym = characters[0]
+
+    # iterate over string
+    #for i in range(1, len(characters)):
+       # if characters[i - 1] == ' ' or characters[i - 1] == '\t':
+            # add letter next to space
+           # acronym += characters[i]
+    #---------------end original code---------------------------------------------------#
+    acronym = [char[0] for char in characters.split()]
+
+    return "".join(acronym).upper()
 
 
 #def get_safe(sentence, key):
--- main.py
+++ main.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     user_input = input("Hello please enter a word or sentence: \n")
     print("There are", get_num_of_characters(user_input), "characters in the text you inputted")
     print("This is your input without spaces or tabs:", output_without_whitespace(user_input))
+    print("This is your input as an acronym:", get_acronym(user_input))
 
